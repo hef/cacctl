af788cc2c50b901e755252664cf3ecfee9ed7caf
hef <hef@pbrfrat.com>
2021-12-09 00:15:30 -0600

    fix(client): remove extra connection in login

--- internal/client/login.go
+++ internal/client/login.go
@@ -15,20 +15,13 @@ func (c *Client) login(ctx context.Context) (*LoginResponse, error) {
 
 func (c *Client) loginWithUsernameAndPassword(ctx context.Context, username, password string) (*LoginResponse, error) {
 
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, "https://panel.cloudatcost.com/login.php", nil)
-	req.Header.Set("User-Agent", c.userAgent)
-	if err != nil {
-		return nil, err
-	}
-	c.httpClient.Do(req)
-
 	form := url.Values{}
 	form.Add("username", username)
 	form.Add("password", password)
 	form.Add("failedpage", "login-failed-2.php")
 	form.Add("submit", "Login")
 
-	req, err = c.newRequest(ctx, http.MethodPost, "https://panel.cloudatcost.com/manage-check2.php", &form)
+	req, err := c.newRequest(ctx, http.MethodPost, "https://panel.cloudatcost.com/manage-check2.php", &form)
 	_, err = c.httpClient.Do(req) // todo: check response for failed password
 	if err != nil {
 		return nil, err
