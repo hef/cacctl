a6238090aa7401a8bac9093cb0feff5e4e8a3409
hef <hef@pbrfrat.com>
2021-12-12 01:44:34 -0600

    wip: checking in wip ssh-copy-id

--- cmd/ssh_copy_id.go
+++ cmd/ssh_copy_id.go
@@ -1,10 +1,18 @@
-//go:build dev
-
 package cmd
 
 import (
+	"context"
+	"fmt"
+	"github.com/hef/cacctl/internal/client"
+	"github.com/pkg/sftp"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"golang.org/x/crypto/ssh"
+	"io"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func init() {
@@ -15,8 +23,64 @@ func init() {
 
 var sshCopyIdCmd = &cobra.Command{
 	Use:   "ssh-copy-id",
-	Short: "List all Servers",
+	Short: "deploy ssh keys to servers",
 	Run: func(cmd *cobra.Command, args []string) {
 
+		ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT)
+		defer cancel()
+
+		c, err := client.New(
+			client.WithUsernameAndPassword(
+				viper.GetString("username"),
+				viper.GetString("password"),
+			),
+			client.WithUserAgent("cacctl/"+Version),
+		)
+		if err != nil {
+			panic(err)
+		}
+
+		serverList, err := c.List(ctx)
+		if err != nil {
+			log.Printf("Failed to get server list")
+		}
+
+		for _, server := range serverList.Servers {
+			err = sshCopyId(ctx, &server)
+		}
 	},
+}
+
+func sshCopyId(ctx context.Context, server *client.Server) error {
+	ip := server.IpAddress
+	config := ssh.ClientConfig{
+		User: "root",
+		Auth: []ssh.AuthMethod{
+			ssh.Password(server.Password),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+	client, err := ssh.Dial("tcp", fmt.Sprintf("%s:%d", ip.String(), 22), &config)
+	if err != nil {
+		return err
+	}
+	defer client.Close()
+
+	session, err := client.NewSession()
+	if err != nil {
+		return err
+	}
+	defer session.Close()
+
+	sftpClient, err := sftp.NewClient(client)
+	if err != nil {
+		return err
+	}
+
+	f, err := sftpClient.Open(".profile")
+	if err != nil {
+		return err
+	}
+	io.Copy(os.Stdout, f)
+	return nil
 }
--- go.mod
+++ go.mod
@@ -4,8 +4,10 @@ go 1.17
 
 require (
 	github.com/PuerkitoBio/goquery v1.8.0
+	github.com/pkg/sftp v1.13.4
 	github.com/spf13/cobra v1.2.1
 	github.com/spf13/viper v1.9.0
+	golang.org/x/crypto v0.0.0-20211209193657-4570a0811e8b
 )
 
 require (
@@ -13,17 +15,19 @@ require (
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/kr/fs v0.1.0 // indirect
 	github.com/magiconair/properties v1.8.5 // indirect
-	github.com/mitchellh/mapstructure v1.4.2 // indirect
+	github.com/mitchellh/mapstructure v1.4.3 // indirect
 	github.com/pelletier/go-toml v1.9.4 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/spf13/afero v1.6.0 // indirect
 	github.com/spf13/cast v1.4.1 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
-	golang.org/x/net v0.0.0-20210916014120-12bc252f5db8 // indirect
-	golang.org/x/sys v0.0.0-20210823070655-63515b42dcdf // indirect
-	golang.org/x/text v0.3.6 // indirect
-	gopkg.in/ini.v1 v1.63.2 // indirect
+	golang.org/x/net v0.0.0-20211209124913-491a49abca63 // indirect
+	golang.org/x/sys v0.0.0-20211210111614-af8b64212486 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	gopkg.in/ini.v1 v1.66.2 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
