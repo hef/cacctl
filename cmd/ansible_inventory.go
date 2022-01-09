79b4ab78062bed66553ba2b9c3798e42058194de
hef <hef@pbrfrat.com>
2022-01-08 21:34:14 -0600

    fix(log): adding log to ssh-copy-id
    You can now see when a key is copied successfully

--- cmd/ansible_inventory.go
+++ cmd/ansible_inventory.go
@@ -2,16 +2,95 @@
 
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"context"
+	"fmt"
+	"github.com/hef/cacctl/internal/client"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"text/template"
+)
 
 func init() {
 	rootCmd.AddCommand(ansibleInventoryCmd)
+	ansibleInventoryCmd.PersistentFlags().IntP("masters", "m", 3, "Number of Masters, default 3")
+	viper.BindPFlag("masters", ansibleInventoryCmd.PersistentFlags().Lookup("masters"))
 }
+
+var hostsTemplate = `---
+kubernetes:
+  children:
+    master:
+      hosts:
+        {{- range .Masters }}
+        cac-{{.ServerId}}:
+          ansible_host: {{.IpAddress}}
+        {{- end}}
+    worker:
+      hosts:
+        {{- range .Workers }}
+        cac-{{.ServerId}}:
+          ansible_host: {{.IpAddress}}
+        {{- end}}
+`
 
 var ansibleInventoryCmd = &cobra.Command{
 	Use:   "ansible-inventory",
 	Short: "create ansible inventory files",
 	Run: func(cmd *cobra.Command, args []string) {
+
+		if viper.GetString("username") == "" || viper.GetString("password") == "" {
+			fmt.Printf("Set a username and password with --username and --password ")
+			fmt.Printf("or by setting the environment variables CAC_USERNAME and CAC_PASSWORD.\n")
+			os.Exit(1)
+		}
+
+		ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT)
+		defer cancel()
+
+		c, err := client.New(
+			client.WithUsernameAndPassword(
+				viper.GetString("username"),
+				viper.GetString("password"),
+			),
+			client.WithUserAgent("cacctl/"+Version),
+		)
+		if err != nil {
+			panic(err)
+		}
+
+		response, err := c.List(ctx)
+		if err != nil {
+			log.Printf("error: %s", err)
+			return
+		}
+
+		tmpl, err := template.New("hosts.yaml").Parse(hostsTemplate)
+		if err != nil {
+			log.Printf("error parsing hosts template: %s", err)
+			return
+		}
+
+		masters := viper.GetInt("masters")
+		if masters > len(response.Servers) {
+			masters = len(response.Servers)
+		}
+		data := struct {
+			Masters []client.Server
+			Workers []client.Server
+		}{
+			Masters: response.Servers[:masters],
+			Workers: response.Servers[masters:],
+		}
+
+		err = tmpl.Execute(os.Stdout, data)
+		if err != nil {
+			log.Printf("error executing host template: %s", err)
+		}
 
 	},
 }
