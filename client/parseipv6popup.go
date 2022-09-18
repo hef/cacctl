e10a6d8f2c6c5c3506422c2db2a12f64f7192b94
hef <hef@pbrfrat.com>
2022-09-18 11:08:05 -0500

    feat(client): adding parsing of ipv6 data

--- client/parseipv6popup.go
+++ client/parseipv6popup.go
@@ -1,16 +1,54 @@
 package client
 
-import "github.com/PuerkitoBio/goquery"
+import (
+	"errors"
+	"github.com/PuerkitoBio/goquery"
+	"io"
+	"net"
+	"strings"
+)
 
-func parseIpv6Popup(reader io.Reader) (string, error) {
+type parsedIpv6Popup struct {
+	Ipv6Address net.IPNet
+	Ipv6Gateway net.IP
+}
+
+func parseIpv6Popup(reader io.Reader) (*parsedIpv6Popup, error) {
 	doc, err := goquery.NewDocumentFromReader(reader)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
-	var ipv6 string
-	doc.Find("input[name=ipv6]").First().Each(func(i int, s *goquery.Selection) {
-		ipv6, _ = s.Attr("value")
-	})
-	return ipv6, nil
+	/* doc.Find("body").Contents().Each(func(i int, s *goquery.Selection) {
+		if goquery.NodeName(s) == "#text" {
+			fmt.Printf(">>> (%d) >>> %s\n", i, s.Text())
+		}
+	})*/
+
+	nodes := doc.Find("body").Contents().Nodes
+	if len(nodes) <= 11 {
+		return nil, errors.New("could not parse ipv6 popup")
+	}
+
+	ipv6AddrString := strings.TrimSpace(nodes[7].Data)
+
+	ip, mask, err := net.ParseCIDR(ipv6AddrString)
+	if err != nil {
+		return nil, err
+	}
+
+	ipv6address := net.IPNet{
+		IP:   ip,
+		Mask: mask.Mask,
+	}
+
+	ipv6GatewayString := strings.TrimSpace(nodes[11].Data)
+
+	gateway := net.ParseIP(ipv6GatewayString)
+
+	form := parsedIpv6Popup{
+		Ipv6Address: ipv6address,
+		Ipv6Gateway: gateway,
+	}
+	return &form, nil
 }
--- client/parseipv6popup_test.go
+++ client/parseipv6popup_test.go
@@ -1,1 +1,48 @@
 package client
+
+import (
+	"bytes"
+	"io"
+	"os"
+	"testing"
+)
+
+func TestParseIpv6Popup(t *testing.T) {
+	f, err := os.Open("testdata/ipv6_popup.html")
+	if err != nil {
+		panic(err)
+	}
+
+	form, err := parseIpv6Popup(f)
+	if err != nil {
+		t.Errorf("error parsing build page: %s", err)
+	}
+
+	expectedAddress := "2607:8880::1830:9d/120"
+
+	if expectedAddress != form.Ipv6Address.String() {
+		t.Errorf("expected ipv6 address %s, got %s", expectedAddress, form.Ipv6Address.String())
+	}
+
+	expectedGateway := "2607:8880::1830:1"
+
+	if expectedGateway != form.Ipv6Gateway.String() {
+		t.Errorf("expected ipv6 gateway %s, got %s", expectedGateway, form.Ipv6Gateway.String())
+	}
+
+}
+
+func FuzzParseIpv6Popup(f *testing.F) {
+	c, err := os.Open("testdata/ipv6_popup.html")
+	if err != nil {
+		panic(err)
+	}
+	sample, err := io.ReadAll(c)
+	if err != nil {
+		panic(err)
+	}
+	f.Add(sample)
+	f.Fuzz(func(t *testing.T, data []byte) {
+		parseIpv6Popup(bytes.NewReader(data))
+	})
+}
